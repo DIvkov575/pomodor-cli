0baced75e4797b706edde3fd8a9d8de28d5625ec
Dima I <79413560+DIvkov575@users.noreply.github.com>
2023-05-21 19:53:04 -0400

    Update (make comments) main.go

--- main.go
+++ main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-  "os"
-  "fmt"
-	"strconv"
-
-  "github.com/spf13/cobra"
-	mcobra "github.com/muesli/mango-cobra"
-  "github.com/spf13/viper"
-  "github.com/muesli/roff"
-
-
+    "os" // used for io
+    "fmt" // print statements
+    "strconv" // strings
+    "github.com/spf13/cobra" // cli lib
+    mcobra "github.com/muesli/mango-cobra"
+    "github.com/spf13/viper" // load config from yaml not toml :(                                                                                    
+    "github.com/muesli/roff" // idk what this is but here it is ig
 )
 
 var (
@@ -20,7 +17,7 @@ var (
   cycle_ctr   = 0
   source      =""
 )
-
+// man command - this does not work and im not fixing it
 var manCmd = &cobra.Command{
 	Use:                   "man",
 	Short:                 "Generates man pages",
@@ -38,10 +35,10 @@ var manCmd = &cobra.Command{
 		return err
 	},
 }
-
+// gets configuration from config file created in install.sh                      
 func getConf(source string) ([]string, []string, bool) {
   viper.SetConfigName("pomodoro-config") // name of config file (without extension)
-  viper.AddConfigPath(source)
+  viper.AddConfigPath(source)                                 
   viper.SetConfigType("yaml")
   err := viper.ReadInConfig() // Find and read the config file
   if err != nil { // Handle errors reading the config file
@@ -53,17 +50,21 @@ func getConf(source string) ([]string, []string, bool) {
 
   return cycle_lengths, cycle_names, confirm_new
 }
-
+                                                                                    
+// creates actuall timer . . .                                   
 var rootCmd = &cobra.Command{
   Args:         cobra.ExactArgs(1),
-  RunE: func(cmd *cobra.Command, args []string) error {
+  RunE: func(cmd *cobra.Command, args []string) error { 
     if (args[0] == "run") {
       fmt.Println("run")
     }
-    if (source == "") {
-      source, _ =  os.UserHomeDir()
-    }
+    // get config file location                                                                                                                   
+    if (source == "") {  
+        source, _ =  os.UserHomeDir()  
+    }        
     cycle_lengths, cycle_names, confirm_new := getConf(source)
+
+     // creates pomodoro timer according to default values (in config)
     if (cycles != "") {
       cycle_num, err := strconv.ParseInt(cycles, 0, 64 )
       if (err != nil) {
@@ -78,6 +79,7 @@ var rootCmd = &cobra.Command{
           interlude(confirm_new)
         }
       }
+	    // creates pomodoro timer according to values passed
     } else {
         for {
           for j := 0; j < len(cycle_lengths); j++ {
@@ -86,25 +88,25 @@ var rootCmd = &cobra.Command{
             timerA(duration, "[ " + label + " cycle " + strconv.Itoa(cycle_ctr) + " ]")
             notification(label+" ended", "")
             interlude(confirm_new)
-  cycle_ctr += 1;
-  fmt.Print(cycle_ctr)
+            cycle_ctr += 1;
+            fmt.Print(cycle_ctr)
           }
         }
       }
     return nil
     },
 }
-
+// helf function, but is also broken/incomplete/useless because I didnt feel like implementing it
 func help() {
   fmt.Println("help stuff1")
 }
-
+// execute before main (allows user to assign flags / pass custom values)    
 func init() {
   rootCmd.Flags().StringVarP(&cycles, "cycles", "c", "", "Number of Cycles")
   rootCmd.Flags().StringVarP(&source, "config path", "s", "", "Path to dir containing config.yaml file")
 }
 
-
+// Entry point            
 func main() {
   rootCmd.Execute() 
   }
